8acc5c896b1b5021fd55eb5da59a410f83571671
Maggie-karuri <karurimargaret111@gmail.com>
2024-05-06 11:09:13 +0300

    bot army enlisting updated

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,29 +1,22 @@
-import React from "react";
-import BotCard from "./BotCard";
+import React from 'react'
+import BotCard from "../components/BotCard"
 
-function YourBotArmy({ bots, removeBot, deleteBot }) {
-  //your bot army code here...
-  const armyItem = bots.map((bot) => {
-    return (
-      <BotCard
-        key={bot.id}
-        bot={bot}
-        clickEvent={removeBot}
-        deleteBot={deleteBot}
-      />
-    );
-  });
+function YourBotArmy(props) {
+
+  const displayBots = props.bots.map(bot => {
+    return <BotCard bot={bot} action={props.action} removeCard={props.removeCard} />
+  })
+
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          {armyItem}
+          {displayBots}
         </div>
       </div>
     </div>
-  );
-}
+  )
 
+}
 export default YourBotArmy;
